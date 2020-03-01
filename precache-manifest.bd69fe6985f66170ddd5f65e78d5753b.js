f7823173901d8f456771c21e67e3f41b7f08cd59
MayjentaMalzahn <42015266+MayMalzahn@users.noreply.github.com>
2020-03-01 18:53:02 -0500

    Updates

---
+++ precache-manifest.bd69fe6985f66170ddd5f65e78d5753b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8cc29a99a853089fcca62a167a022d",
+    "url": "/azeroth-heros/index.html"
+  },
+  {
+    "revision": "050d47673a45de7147e0",
+    "url": "/azeroth-heros/static/css/main.3dd3c4bf.chunk.css"
+  },
+  {
+    "revision": "6b081eaed9b9d6a77102",
+    "url": "/azeroth-heros/static/js/2.73fd7766.chunk.js"
+  },
+  {
+    "revision": "9ae78a31079e09c5b941064d353368c2",
+    "url": "/azeroth-heros/static/js/2.73fd7766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050d47673a45de7147e0",
+    "url": "/azeroth-heros/static/js/main.0c1c7b3c.chunk.js"
+  },
+  {
+    "revision": "14ace9f3bc550bbb52c6",
+    "url": "/azeroth-heros/static/js/runtime-main.abf2e8f0.js"
+  },
+  {
+    "revision": "1d522baf7a8745b1485ecc0dc202bdfe",
+    "url": "/azeroth-heros/static/media/dps.1d522baf.png"
+  },
+  {
+    "revision": "548ed70ae20d08f926d513cae8b99087",
+    "url": "/azeroth-heros/static/media/heal.548ed70a.png"
+  },
+  {
+    "revision": "95a8f8472eb44587214459ef7f0feae8",
+    "url": "/azeroth-heros/static/media/home-img.95a8f847.png"
+  },
+  {
+    "revision": "bacbaba75974d72908e87aceae30d437",
+    "url": "/azeroth-heros/static/media/logo.bacbaba7.jpg"
+  },
+  {
+    "revision": "1061efffe43391f93842bc4955407f11",
+    "url": "/azeroth-heros/static/media/tank.1061efff.png"
+  }
+]);
